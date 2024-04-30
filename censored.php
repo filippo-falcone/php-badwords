60acfe0480b0b04f289285c069b1938459b8ff0b
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-30 14:49:29 +0200

    feat: added censored.php html and style

---
+++ censored.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bardwords</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>PHP Badwords</h1>
+        <h2>Paragrafo</h2>
+        <div class="card">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, tempora. Quia, aliquid consequuntur ab repudiandae ex veniam tenetur corporis explicabo repellat rerum, magni nam temporibus similique. Ut ratione odit quo?</p>
+            <p>Lunghezza paragrafo: [Number]</p>
+        </div>
+        <h2 class="censored">Paragrafo Censurato</h2>
+        <div class="card">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum sunt nulla ducimus praesentium nam, laudantium doloribus voluptate recusandae necessitatibus sequi quis. Minima nobis odit, enim perspiciatis porro sint ipsa saepe.</p>
+        </div>
+    </div>
+</body>
+</html>
